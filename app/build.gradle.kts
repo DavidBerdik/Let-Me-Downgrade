91ee47ffa5401e5bad08e07881132548a7a6abf5
David Berdik <berdikgit@theprogrammersworld.net>
2026-06-21 17:48:37 -0400

    Changed version code from 9 to 11 and version name from 1.0.6 to 1.1.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     defaultConfig {
         minSdk = 31
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.0.6"
+        versionCode = 11
+        versionName = "1.1.0"
         applicationId = "com.berdik.letmedowngrade"
     }
 
